62af5707e54ed686528bf9417b93773971265ed7
Arthur Le Guennec <arthur.leguennec@gmail.com>
2017-10-19 12:31:50 +0200

    Step 1 is step 2 now and the step 2 will compute features

--- step2.py
+++ step2.py
@@ -1,27 +1,30 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Oct 19 12:18:56 2017
+Created on Wed Oct 18 11:56:58 2017
 
 @author: Arthur Le Guennec
 """
 
 ###########################################################
-###        STEP ONE                                     ###
-###   Script to compute feature on raster               ###
+###        STEP TWO                                     ###
+###   Script to rasterize las or laz file and train     ###
+###   a random forest to disting water surface to       ###
+###   ground to some stuff                              ###
 ###########################################################
 
-## Goals:   - compute feature on raster
-##          - use several scales (radiuses: 0.25 to 7 meters)
+## Goals:   - train a model with raster
+##          - find the best scale range to separe water, ground and the rest
 
 ## Step:    1) load raster laz files
 ##          2) load laz files
 ##          3) compute descriptors on raster with the original file
+##          4) keep only ground and water surface points with a good confidence
+##             (like >= 0.9)
 
 from laspy.file import File
 from math import atan2
 from sklearn.neighbors import KDTree
 from useful_functions.function_las import *
-from useful_functions.function_canupo import *
 #from useful_functions.function_figures import *
 #from useful_functions.function_canupo import *
 #from useful_functions.function_random_forest import *
@@ -37,13 +40,16 @@ import os
 import sys
 
 
+
+
+
 def main():
     dir_raster_in = "../../data/rasterized_tiles/"
     dir_in = "../../data/labeled_tiles/"
 
     scales = [0.25, 0.5, 1, 2, 2.5, 3, 4, 5, 6, 7]
 
-    f = open('train_data.txt', 'r')
+    f = open('filename_data.txt', 'r')
     filenames_train_C2 = f.readlines()
     filenames_train_C3 = copy.copy(filenames_train_C2)
     filenames_train_raster_C2 = copy.copy(filenames_train_C2)
@@ -56,6 +62,10 @@ def main():
         filenames_train_C3[i] = foo.replace('C2', 'C3')
         filenames_train_raster_C2[i] = foo.replace('.laz', 
                                                    '_raster_2_5_low.laz')
+
+
+#    filename_test_C2 = ["tile_C2_869000_6523500_raster_2_5_low.laz"]
+#    filename_test_C3 = ["tile_C3_869000_6523500_raster_2_5_low.laz"]
     
     for i in range(len(filenames_train_C2)):
         inFile_C2 = load_las_file(dir_in + filenames_train_C2[i])
@@ -64,29 +74,6 @@ def main():
                                          + filenames_train_raster_C2[i])
         
         
-        cloud_C2 = np.vstack([inFile_C2.x,
-                              inFile_C2.y,
-                              inFile_C2.z,
-                              inFile_C2.classification,
-                              inFile_C2.intensity,
-                              inFile_C2.num_returns,
-                              inFile_C2.return_num]).transpose()
-
-        global cloud_C3
-        cloud_C3 = np.vstack([inFile_C3.x,
-                              inFile_C3.y,
-                              inFile_C3.z,
-                              inFile_C3.classification,
-                              inFile_C3.intensity,
-                              inFile_C3.num_returns,
-                              inFile_C3.return_num]).transpose()
-        
-        
-        print("Calcul KDTree C2")
-        kdtree_C2 = KDTree(coord_C2, leaf_size=40)
-        global kdtree_C3
-        print("Calcul KDTree C3")
-        kdtree_C3 = KDTree(coord_C3, leaf_size=40)
 #        if (filename == filenames_train_C2[0]):
 #            nam_feat = name_features.copy()
 #
@@ -100,8 +87,8 @@ def main():
 #            labels_train = np.concatenate([labels_train, lab_tmp])
     
     return
-    
-    
+
+
 
 if __name__ == "__main__":
     main()
