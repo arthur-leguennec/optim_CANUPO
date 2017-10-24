f246a0f2ad80b3aa0e99a980eaacb8dca43467c2
Arthur Le Guennec <arthur.leguennec@gmail.com>
2017-10-24 17:12:44 +0200

    TODO: Need to modify functions. Functions must not take kdtree

--- useful_functions/function_canupo.py
+++ useful_functions/function_canupo.py
@@ -16,15 +16,14 @@ import scipy.linalg as la
 import copy
 
 
-def compute_pca_for_corepoint(kdtree, 
-                              point,
+def compute_pca_for_corepoint(index,
                               coordinates,
                               radius,
                               verbose=False):
     """Compute PCA descriptors around one point with several scales.
     
     Args:
-        kdtree: KDTree computed with sklearn.neighbors.
+        index: Index inside the ball (replace KDTree)
         point: the point where we will compute features. Have to be a 
                coordinate
         coordinates: list of coordinates.
@@ -46,39 +45,20 @@ def compute_pca_for_corepoint(kdtree,
         >> intensity = points[:, 3]
         >> coord = points[:, 0:3]
         >> kdtree = KDTree(coord, leaf_size=40)
-        >> kdtree
-        >> scales_features = compute_pca_for_corepoint(kdtree=kdtree,
+        >> index = kdtree.query_radius(coord[10, :], 2)[0]
+        >> scales_features = compute_pca_for_corepoint(index,
                                                        coord[10, :],
-                                                       [1, 2, 3])
+                                                       coord)
     """
-    point_tmp = np.array([point])
-    point_tmp = point_tmp.reshape(1, -1)
-    ind, dist = kdtree.query_radius(point_tmp,
-                                    np.array([radius]),
-                                    return_distance=True,
-                                    sort_results=True)
-    ind = ind[0]
-    dist = dist[0]
-
     lambda1s = []
     lambda2s = []
     lambda3s = []
     slopes = []
-    ind_in_radius = []
-
-    # tmp is a list of indices of indices ...
-    tmp = np.argwhere(dist < radius).reshape(-1)
-    ind_in_radius.append(ind[tmp])
-
-    # How many points we have in the sphere ?
-    nb_pts_radius = 0
-    for ind in ind_in_radius:
-        nb_pts_radius = nb_pts_radius + len(ind)
 
     [lambda1, 
      lambda2, 
      lambda3, 
-     slope] = compute_pca(coordinates[ind_in_radius])
+     slope] = compute_pca(coordinates[index])
     lambda1s.append(lambda1)
     lambda2s.append(lambda2)
     lambda3s.append(lambda3)
@@ -130,21 +110,14 @@ def compute_pca(coord):
     return [lambda1, lambda2, lambda3, slope]
 
 
-def compute_feature_for_corepoint(kdtree, 
-                                  point, 
-                                  feature, 
-                                  radius, 
+def compute_feature_for_corepoint(feature,
                                   option='mean',
                                   verbose=False):
-    """Compute descriptors around one point with several scales. According to
-    parameters, it's possible to used several point cloud (C2 and C3 here)
+    """Compute descriptors around one point with several scales.
     
     Args:
-        kdtree: KDTree computed with sklearn.neighbors
-        point: the point where we will compute features. Have to be a 
-               coordinate
+        points: numpy array
         feature: numpy array of the concerned features
-        radius: size of desired neighborhood
         option: what kind of feature we want, by default it's the mean
                 (possibility: mean or std)
         verbose: Controls the verbosity of the function. False by default.
@@ -153,29 +126,18 @@ def compute_feature_for_corepoint(kdtree,
         >> from laspy.file import File
         >> from sklearn.neighbors import KDTree
         >> import numpy as np
-        >> inFile_C2 = File("input_C2.laz", mode = "r")
-        >> inFile_C3 = File("input_C3.laz", mode = "r")
-        >> cloud_C2 = np.vstack([inFile_C2.x,
-                                 inFile_C2.y,
-                                 inFile_C2.z]).transpose()
-        >> cloud_C3 = np.vstack([inFile_C3.x,
-                                 inFile_C3.y,
-                                 inFile_C3.z,
-                                 inFile_C3.intensity]).transpose()
-        >> points_C2 = np.require(cloud_C2, 
-                                  dtype=np.float64, 
-                                  requirements=['C', 'A'])
-        >> points_C3 = np.require(cloud_C3, 
-                                  dtype=np.float64, 
-                                  requirements=['C', 'A'])
-        >> in_C3 = points_C3[:, 3]
-        >> coord_C2 = points_C2[:, 0:3]
-        >> coord_C3 = points_C3[:, 0:3]
-        >> kdtree_C3 = KDTree(coord_C3, leaf_size=40)
-        >> kdtree
-        >> scales_features = compute_feature_for_corepoint(kdtree_C3,
-                                                           coord_C2[10, :],
-                                                           in_C3,
+        >> inFile = File("input.laz", mode = "r")
+        >> cloud = np.vstack([inFile.x,
+                             inFile.y,
+                             inFile.z]).transpose()
+        >> points = np.require(cloud, 
+                               dtype=np.float64, 
+                               requirements=['C', 'A'])
+        >> intensity = points_C3[:, 3]
+        >> coord = points[:, 0:3]
+        >> kdtree = KDTree(coord, leaf_size=40)
+        >> index = kdtree.query_radius(coord[10, :], 2)[0]
+        >> scales_features = compute_feature_for_corepoint(intensity[index],
                                                            radius=2,
                                                            option='std')
     """
